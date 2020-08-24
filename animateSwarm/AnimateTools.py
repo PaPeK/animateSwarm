876682f0b9b6b0edbd78afb4f158abe6f9507323
klamser <klamser@physik.hu-berlin.de>
2020-08-24 15:47:38 +0200

    added copy-right and description 	modified:   animateSwarm/AnimateTools.py

--- animateSwarm/AnimateTools.py
+++ animateSwarm/AnimateTools.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+'''
+    AnimateTools
+    Library which has a simple and extendable way to animate
+    agent-based simulation of active-matter (swarms, colloids, flocks....).
+    The animation can be saved as movies, gifs, images of can just be displayed.
+    v0.1, 24.8.2020
+
+    (C) 2020 Pascal Klamser
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 from time import sleep
 from scipy import spatial
 import numpy as np
